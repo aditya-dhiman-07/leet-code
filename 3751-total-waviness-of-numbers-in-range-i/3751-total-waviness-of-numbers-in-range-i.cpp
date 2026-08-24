b6905c4ea74f62bf7c00b11823c8a2da820bba56
aditya-dhiman-07 <addhiman245@gmail.com>
2026-08-24 11:57:49 +0530

    Time: 3 ms (94.54%), Space: 8.7 MB (91.28%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -1,19 +1,39 @@
-class Solution {
-public:
-    int totalWaviness(int num1, int num2) {
-        int ans = 0;
-
-        for (int x = num1; x <= num2; x++) {
-            string s = to_string(x);
-
-            for (int i = 1; i + 1 < s.size(); i++) {
-                if ((s[i] > s[i - 1] && s[i] > s[i + 1]) ||
-                    (s[i] < s[i - 1] && s[i] < s[i + 1])) {
-                    ans++;
-                }
-            }
-        }
-
-        return ans;
-    }
+class Solution { 
+public: 
+    int totalWaviness(int num1, int num2) { 
+        
+        if(num1 < 100 && num2 >= 120) { 
+            num1 = 101; 
+        } 
+        
+        if(num1 < 100 && num2 < 100) { 
+            return 0; 
+        } 
+        
+        int sum = 0; 
+        
+        for(int i = num1; i <= num2; i++) { 
+            int t = i; 
+            
+            int temp1 = t % 10; 
+            t = t / 10; 
+            
+            while(t > 9) { 
+                int temp2 = t % 10; 
+                t = t / 10; 
+                
+                if(temp2 > temp1 && temp2 > t % 10) { 
+                    sum++; 
+                } 
+                
+                if(temp2 < temp1 && temp2 < t % 10) { 
+                    sum++; 
+                } 
+                
+                temp1 = temp2; 
+            } 
+        } 
+        
+        return sum; 
+    } 
 };
